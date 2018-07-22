8909cf4e482687d49f82d736de8163892494f8f9
Michael Morgan <michael1986morgan@gmail.com>
2018-07-22 00:26:42 -0400

    init shoppingList.js file

---
+++ shoppingList.js
@@ -0,0 +1,41 @@
+// ​Create an array to represent your shopping list with the following items: 
+// 'pop tarts', 'ramen noodles', 'chips', 'salsa', and 'coffee'.
+// Add 'fruit loops' to the list.
+// Update 'coffee' to 'fair trade coffee'
+// Replace 'chips' and 'salsa' with 'rice' and 'beans'
+// Create an empty array to represent your shopping cart.
+// Remove the last item from your shopping list and add it to your cart
+// Remove the first item from your shopping list and add it to the cart
+// Write a 'while' loop that takes items from your shopping list and moves 
+// them to your cart until there are no items left on the list.
+// Sort the items in your cart alphabetically... backwards
+// Print the list of items in your shopping cart to the console as comma separated string.
+
+let shoppingList = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee']
+
+// UPDATING SHOPPING LIST
+shoppingList.push('fruit loops')
+shoppingList[4] = 'fair trade coffee'
+shoppingList.splice(2, 2, 'rice', 'beans')
+
+// REMOVE LAST ITEM FROM SHOPPING LIST AND ADD TO CART
+// ADDING ITEMS TO CART
+let shoppingCart = []
+shoppingCart.push(shoppingList.pop())
+shoppingCart.push(shoppingList.shift())
+
+
+
+
+// while loop
+let i = 0;
+while (i < shoppingList.length) {
+   shoppingCart.push(shoppingList[i]);
+   i++;
+}
+
+
+// sort items alphabetically backwards
+shoppingCart.sort().reverse()
+
+console.log('My shopping car contains: ', shoppingCart.join(', '))
